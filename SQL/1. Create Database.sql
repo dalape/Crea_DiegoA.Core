eaaa3f9c297081e42f3ed6eeacb8ecb0077b9c31
dalape <69727672+dalape@users.noreply.github.com>
2021-01-06 09:33:50 -0500

    Update 1. Create Database.sql
    update language in file 1. Create Database.sql

--- SQL/1. Create Database.sql
+++ SQL/1. Create Database.sql
@@ -1,7 +1,7 @@
-print '--****** Creación de base de datos para la prueba de Crea sistemas. Realizada por Diego Alape --******'
-print '***** Inicio de ejecución ' + Convert(varchar(50), getdate()) + ' *****'
+print '--****** database creation --******'
+print '***** Start of execution ' + Convert(varchar(50), getdate()) + ' *****'
 If not Exists(SELECT name FROM master.sys.databases WHERE name = 'Crea_Test_DA') Begin
-	print '**** Creando la base de Datos ****'
+	print '**** creating database ****'
 	Create database Crea_Test_DA
 End
-print '***** Fin de ejecución ' + Convert(varchar(50), getdate()) + ' *****'
+print '***** end of execution ' + Convert(varchar(50), getdate()) + ' *****'
